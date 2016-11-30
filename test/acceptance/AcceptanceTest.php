eea2152700846878a77976351e12be33afd2524e
fqqdk <fqqdk1@gmail.com>
2016-11-30 15:40:53 +0100

    get logs of api stub service
    logging to file from test

--- test/acceptance/AcceptanceTest.php
+++ test/acceptance/AcceptanceTest.php
@@ -3,6 +3,8 @@
 namespace Suite\Api\Acceptance;
 
 use Emartech\TestHelper\BaseTestCase;
+use Monolog\Handler\StreamHandler;
+use Monolog\Logger;
 use PHPUnit_Framework_Constraint;
 
 use Suite\Api\Client;
@@ -16,8 +18,9 @@ class AcceptanceTest extends BaseTestCase
      */
     public function sendingRequestWorks()
     {
+        $spyLogger = new Logger('spy', [new StreamHandler(fopen('/tmp/php-suite-api.log', 'a'))]);
         $escherProvider = new EscherProvider('foo/bar/baz', 'key', 'secret', []);
-        $client = Client::create($this->dummyLogger, $escherProvider, new SuiteResponseProcessor($this->dummyLogger));
+        $client = Client::create($spyLogger, $escherProvider, new SuiteResponseProcessor($spyLogger));
 
         $this->assertThat($client->get('http://localhost:7984/'), $this->isSuccessFulApiResponse());
     }
--- test/bootstrap.php
+++ test/bootstrap.php
@@ -2,7 +2,7 @@
 
 call_user_func(function () {
     // start serving suite API stub
-    exec('php -S localhost:7984 '.__DIR__.'/acceptance/api-stub.php >/dev/null 2>&1 & echo $!', $output);
+    exec('php -S localhost:7984 '.__DIR__.'/acceptance/api-stub.php >/tmp/php-suite-api.log 2>&1 & echo $!', $output);
     $pid = (int) $output[0];
     echo "Starting API stub" . PHP_EOL;
 
@@ -10,5 +10,6 @@ call_user_func(function () {
     register_shutdown_function(function() use ($pid) {
         echo "Stopping API stub" . PHP_EOL;
         exec('kill ' . $pid);
+        echo file_get_contents('/tmp/php-suite-api.log');
     });
 });
