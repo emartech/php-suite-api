9f55a18e10867f006081fa40bb20bb4137d9d1f2
fqqdk <fqqdk1@gmail.com>
2016-12-15 13:04:44 +0100

    use silex for API stub

--- test/acceptance/AcceptanceTest.php
+++ test/acceptance/AcceptanceTest.php
@@ -7,33 +7,67 @@ use Monolog\Handler\StreamHandler;
 use Monolog\Logger;
 use PHPUnit_Framework_Constraint;
 
+use Psr\Log\LoggerInterface;
 use Suite\Api\Client;
+use Suite\Api\Error;
 use Suite\Api\EscherProvider;
 use Suite\Api\SuiteResponseProcessor;
 
 class AcceptanceTest extends BaseTestCase
 {
     /**
+     * @var LoggerInterface
+     */
+    private $spyLogger;
+
+    protected function setUp()
+    {
+        parent::setUp();
+        $this->spyLogger = new Logger('spy', [new StreamHandler(fopen('/tmp/php-suite-api.log', 'a'))]);
+    }
+
+    /**
      * @test
      */
     public function sendingRequestWorks()
     {
-        $spyLogger = new Logger('spy', [new StreamHandler(fopen('/tmp/php-suite-api.log', 'a'))]);
-        $escherProvider = new EscherProvider('foo/bar/baz', 'key', 'secret', []);
-        $client = Client::create($spyLogger, $escherProvider, new SuiteResponseProcessor($spyLogger));
+        $client = $this->createClient(new EscherProvider('foo/bar/baz', 'key', 'secret', []));
 
-        $this->assertThat($client->get('http://localhost:7984/'), $this->isSuccessFulApiResponse());
+        $this->assertThat($client->get('http://localhost:7984/'), $this->isSuccessfulApiResponse());
+    }
+    /**
+     * @test
+     */
+    public function authenticationWorks()
+    {
+        $client = $this->createClient(new EscherProvider('foo/bar/invalid_credential', 'key', 'secret', []));
+
+        try {
+            $client->get('http://localhost:7984/');
+            $this->fail('An exception was expected');
+        } catch (Error $exception) {
+            $this->assertEquals('Authentication error.', $exception->getMessage());
+        }
     }
 
     /**
      * @return PHPUnit_Framework_Constraint
      */
-    private function isSuccessFulApiResponse()
+    private function isSuccessfulApiResponse()
     {
         return $this->structure([
             'replyCode' => 0,
-            'replyText' => 'Success.',
-            'success' => true,
+            'replyText' => 'OK',
+            'data' => '',
         ]);
+    }
+
+    /**
+     * @param $escherProvider
+     * @return Client
+     */
+    private function createClient($escherProvider):Client
+    {
+        return Client::create($this->spyLogger, $escherProvider, new SuiteResponseProcessor($this->spyLogger));
     }
 }
--- test/acceptance/api-stub.php
+++ test/acceptance/api-stub.php
@@ -1,15 +1,25 @@
 <?php
 
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
 require_once __DIR__.'/../../vendor/autoload.php';
 
-try {
-    $escherProvider = new \Suite\Api\EscherProvider('foo/bar/baz', 'irrelevant', 'irrelevant', ['key' => 'secret']);
-    $escherProvider->createEscher()->authenticate($escherProvider->getKeyDB());
-    echo json_encode(['replyCode' => 0, 'replyText' => 'Success.']);
-} catch (EscherException $exception) {
-    header('HTTP/1.1 403 Unauthorized');
-    echo json_encode(['replyCode' => 1, 'replyText' => 'Authentication error.']);
-} catch (\Exception $exception) {
-    header('HTTP/1.1 500 Internal Server Error');
-    echo json_encode(['replyCode' => 2, 'replyText' => (string)$exception]);
-}
+$app = new \Silex\Application();
+$app->before(function () {
+    try {
+        $escherProvider = new \Suite\Api\EscherProvider('foo/bar/baz', 'irrelevant', 'irrelevant', ['key' => 'secret']);
+        $escherProvider->createEscher()->authenticate($escherProvider->getKeyDB());
+        return null;
+    } catch (EscherException $exception) {
+        return new Response(json_encode(['replyCode' => 1, 'replyText' => 'Authentication error.', 'data' => '']), 403);
+    } catch (\Exception $exception) {
+        return new Response(json_encode(['replyCode' => 1, 'replyText' => (string)$exception, 'data' => '']), 500);
+    }
+});
+
+$app->get('/', function(Request $request) {
+    return json_encode(['replyCode' => 0, 'replyText' => 'OK', 'data' => '']);
+});
+
+$app->run();
